cf084d851463b179f0927d308d45e02ce436887e
TickTackk <me@ticktackk.io>
2020-06-14 12:50:15 +0530

    Handle moment.js not loaded #2

--- upload/src/addons/SV/NoticeTimeReplacable/_files/js/sv/notice-time-replacable/core.js
+++ upload/src/addons/SV/NoticeTimeReplacable/_files/js/sv/notice-time-replacable/core.js
@@ -1,95 +1,99 @@
 var SV = window.SV || {};
 SV.NoticeTimeReplacable = SV.NoticeTimeReplacable || {};
 
-/**
- * @see https://gist.github.com/phpmypython/f97c5f5f59f2a934599d
- */
-(function (m) {
-    /*
-     * PHP => moment.js
-     * Will take a php date format and convert it into a JS format for moment
-     * http://www.php.net/manual/en/function.date.php
-     * http://momentjs.com/docs/#/displaying/format/
+if (typeof moment === 'function')
+{
+    /**
+     * @see https://gist.github.com/phpmypython/f97c5f5f59f2a934599d
      */
-    var formatMap = {
-            d: 'DD',
-            D: 'ddd',
-            j: 'D',
-            l: 'dddd',
-            N: 'E',
-            S: function ()
-            {
-                return '[' + this.format('Do').replace(/\d*/g, '') + ']';
-            },
-            w: 'd',
-            z: function ()
-            {
-                return this.format('DDD') - 1;
-            },
-            W: 'W',
-            F: 'MMMM',
-            m: 'MM',
-            M: 'MMM',
-            n: 'M',
-            t: function ()
-            {
-                return this.daysInMonth();
-            },
-            L: function ()
-            {
-                return this.isLeapYear() ? 1 : 0;
-            },
-            o: 'GGGG',
-            Y: 'YYYY',
-            y: 'YY',
-            a: 'a',
-            A: 'A',
-            B: function ()
-            {
-                var thisUTC = this.clone().utc(),
-                    // Shamelessly stolen from http://javascript.about.com/library/blswatch.htm
-                    swatch = ((thisUTC.hours() + 1) % 24) + (thisUTC.minutes() / 60) + (thisUTC.seconds() / 3600);
-                return Math.floor(swatch * 1000 / 24);
-            },
-            g: 'h',
-            G: 'H',
-            h: 'hh',
-            H: 'HH',
-            i: 'mm',
-            s: 'ss',
-            u: '[u]', // not sure if moment has this
-            e: '[e]', // moment does not have this
-            I: function () {
-                return this.isDST() ? 1 : 0;
-            },
-            O: 'ZZ',
-            P: 'Z',
-            T: '[T]', // deprecated in moment
-            Z: function () {
-                return parseInt(this.format('ZZ'), 10) * 36;
-            },
-            c: 'YYYY-MM-DD[T]HH:mm:ssZ',
-            r: 'ddd, DD MMM YYYY HH:mm:ss ZZ',
-            U: 'X'
-        },
-        formatEx = /[dDjlNSwzWFmMntLoYyaABgGhHisueIOPTZcrU]/g;
-
-    moment.fn.formatPHP = function (format)
+    (function (m)
     {
-        var that = this;
+        /*
+         * PHP => moment.js
+         * Will take a php date format and convert it into a JS format for moment
+         * http://www.php.net/manual/en/function.date.php
+         * http://momentjs.com/docs/#/displaying/format/
+         */
+        var formatMap = {
+                d: 'DD',
+                D: 'ddd',
+                j: 'D',
+                l: 'dddd',
+                N: 'E',
+                S: function ()
+                {
+                    return '[' + this.format('Do').replace(/\d*/g, '') + ']';
+                },
+                w: 'd',
+                z: function ()
+                {
+                    return this.format('DDD') - 1;
+                },
+                W: 'W',
+                F: 'MMMM',
+                m: 'MM',
+                M: 'MMM',
+                n: 'M',
+                t: function ()
+                {
+                    return this.daysInMonth();
+                },
+                L: function ()
+                {
+                    return this.isLeapYear() ? 1 : 0;
+                },
+                o: 'GGGG',
+                Y: 'YYYY',
+                y: 'YY',
+                a: 'a',
+                A: 'A',
+                B: function ()
+                {
+                    var thisUTC = this.clone().utc(),
+                        // Shamelessly stolen from http://javascript.about.com/library/blswatch.htm
+                        swatch = ((thisUTC.hours() + 1) % 24) + (thisUTC.minutes() / 60) + (thisUTC.seconds() / 3600);
+                    return Math.floor(swatch * 1000 / 24);
+                },
+                g: 'h',
+                G: 'H',
+                h: 'hh',
+                H: 'HH',
+                i: 'mm',
+                s: 'ss',
+                u: '[u]', // not sure if moment has this
+                e: '[e]', // moment does not have this
+                I: function () {
+                    return this.isDST() ? 1 : 0;
+                },
+                O: 'ZZ',
+                P: 'Z',
+                T: '[T]', // deprecated in moment
+                Z: function () {
+                    return parseInt(this.format('ZZ'), 10) * 36;
+                },
+                c: 'YYYY-MM-DD[T]HH:mm:ssZ',
+                r: 'ddd, DD MMM YYYY HH:mm:ss ZZ',
+                U: 'X'
+            },
+            formatEx = /[dDjlNSwzWFmMntLoYyaABgGhHisueIOPTZcrU]/g;
 
-        return this.format(format.replace(formatEx, function (phpStr)
+        moment.fn.formatPHP = function (format)
         {
-            if (typeof formatMap[phpStr] === 'function')
-            {
-                return formatMap[phpStr].call(that);
-            }
+            var that = this;
 
-            console.log(phpStr + '==>' + formatMap[phpStr]);
-            return formatMap[phpStr];
-        }));
-    };
-}(moment));
+            return this.format(format.replace(formatEx, function (phpStr)
+            {
+                if (typeof formatMap[phpStr] === 'function')
+                {
+                    return formatMap[phpStr].call(that);
+                }
+
+                console.log(phpStr + '==>' + formatMap[phpStr]);
+                return formatMap[phpStr];
+            }));
+        };
+    }(moment));
+}
 
 !function($, window, document, _undefined)
 {
@@ -121,6 +125,12 @@ SV.NoticeTimeReplacable = SV.NoticeTimeReplacable || {};
             if (!this.options.timeFormat)
             {
                 console.error('Time format missing.');
+                return;
+            }
+
+            if (typeof moment !== 'function')
+            {
+                console.error('Moment.js not loaded.');
                 return;
             }
 
